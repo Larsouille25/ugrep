295bd029e3ac7b9168c6502132fbca5faf925d20
Larsouille <superthithi2008@gmail.com>
2023-01-24 16:20:13 +0100

    Working has expected, logic

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
+use ugrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,33 +14,8 @@ fn main() {
     println!("Searching for `{}`", config.query);
     println!("In file, `{}`", config.filename);
 
-    if let Err(e) = run(config) {
+    if let Err(e) = ugrep::run(config) {
         println!("Application error: {e}");
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{contents}");
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("Not enought arguments")
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-    
-        Ok(Config {query, filename}) 
     }
 }
