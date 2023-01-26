d5d478156cb046d766e511cf6b06f0f5f468037b
Larsouille <superthithi2008@gmail.com>
2023-01-26 10:47:39 +0100

    Init clip-it branch

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@ use std::process;
 
 use ugrep::Config;
 
+
 fn main() {
     let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-    println!("Searching for `{}`", config.query);
-    println!("In file, `{}`", config.filename);
+    println!("Searching for `{}`, in `{}`...\n", config.query, config.filename);
 
     if let Err(e) = ugrep::run(config) {
         eprintln!("Application error: {e}");
