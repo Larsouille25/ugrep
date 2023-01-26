5cdfbb3697e40dbd58f8678510f59cc0afd05b44
Larsouille <superthithi2008@gmail.com>
2023-01-26 09:27:50 +0100

    Performance optimization

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,10 @@ use std::process;
 use ugrep::Config;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-
     println!("Searching for `{}`", config.query);
     println!("In file, `{}`", config.filename);
 
