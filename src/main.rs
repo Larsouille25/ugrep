ee6fdb196f5817350dc7972c25798babd78bb297
Larsouille <superthithi2008@gmail.com>
2023-01-26 12:28:46 +0100

    Added colors and two flag

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,46 @@
-// use std::env;
 use std::process;
 
 use clap::Parser;
+use colored::Colorize;
 
 use ugrep::Config;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)] // Read from `Cargo.toml`
 struct Cli {
+    // ARGS:
     pattern: String,
     path: String,
-    #[arg(short, long)]
+
+    // FLAGS:
+    #[arg(short, long, action = clap::ArgAction::SetFalse)]
     case_sensitive: bool,
+    #[arg(short, long, action = clap::ArgAction::SetFalse)]
+    only_result: bool,
 }
 
 fn main() {
     let cli = Cli::parse();
-    // println!("pattern: {}", cli.pattern);
-    // println!("path: {}", cli.path);
-    // println!("case_sensitive: {:?}", cli.case_sensitive);
-
-
-
     let config = Config::new(cli.pattern, cli.path, cli.case_sensitive);
-    print!("Searching for `{}`, in `{}`", config.pattern, config.filename);
-    if config.case_sensitive {
-        print!(" with case sensisvity");
+
+    if cli.only_result{
+        let mut str = format!("Searching for `{}`, in `{}`", config.pattern, config.filename);
+        if config.case_sensitive {
+            str.push_str(" with case sensitivity")
+        }
+
+        println!("{} \n", str.italic().bright_black());
     }
-    println!("\n");
+
 
     if let Err(e) = ugrep::run(config) {
         eprintln!("Application error: {e}");
         process::exit(1);
     }
+}
+
+#[test]
+fn verify_cli() {
+    use clap::CommandFactory;
+    Cli::command().debug_assert()
 }
